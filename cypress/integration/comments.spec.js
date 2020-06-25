5684f87c0370ead19e49c0c72fae4b5aac068f32
Daniel Stefanello <danieldfst@hotmail.com>
2020-06-24 22:43:26 -0300

    Add test to the evaluation field

--- cypress/integration/comments.spec.js
+++ cypress/integration/comments.spec.js
@@ -20,39 +20,54 @@ describe('Should be able register a comment', () => {
 
           cy.get('[data-cy=send-commentary]').click();
 
-          cy.get('#ulComments li:last-child > [data-cy=comment-text]').should(
+          cy.get('[data-cy=comment-text]:last').should(
             'have.text',
             item.comment
           );
 
-          cy.get('#ulComments li:last-child > [data-cy=comment-rate]').should(
-            'have.text',
-            item.rate
-          );
+          cy.get('[data-cy=comment-rate]:last').should('have.text', item.rate);
         });
       });
   });
 
   it('Should be increment number of commentaries', () => {
-    cy.get('#count')
+    cy.get('[data-cy=count]')
       .invoke('text')
       .then((count1) => {
-        cy.get('#inComment')
+        cy.get('[data-cy=input-commentary]')
           .type('Aumentando o número de comentários!')
           .should('have.value', 'Aumentando o número de comentários!');
 
-        cy.get('#inRate').select('5').should('have.value', '5');
+        cy.get('[data-cy=input-rate]').select('5').should('have.value', '5');
 
-        cy.get('#sender').click();
+        cy.get('[data-cy=send-commentary]').click();
 
         let result = parseInt(count1);
 
         result++;
 
-        cy.get('#count')
+        cy.get('[data-cy=count]')
           .invoke('text')
           .should((count2) => {
             expect(`${result}`).be.equal(count2);
+          });
+      });
+  });
+
+  it('Should be average correctly value', () => {
+    let sun = 0;
+
+    cy.get('[data-cy=comment-rate]')
+      .each((item) => {
+        cy.get(item)
+          .invoke('text')
+          .then((value) => (sun += parseInt(value)));
+      })
+      .then((result) => {
+        cy.get('[data-cy=average]')
+          .invoke('text')
+          .then((value) => {
+            expect(value).eq((sun / result.length).toFixed(1));
           });
       });
   });
